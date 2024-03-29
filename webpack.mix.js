a775f3fad8d9ae95373c09cd340758d718a72248
Nicolas Munoz <datac.nicolasm@gmail.com>
2024-03-29 00:34:07 -0500

    Agregar plugins de adminlte y fontawesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
+   .setResourceRoot('../')
    .babelConfig({
        presets: ['@babel/preset-env']
    })
@@ -11,3 +12,10 @@ mix.js('resources/js/app.js', 'public/js')
 if (mix.inProduction()) {
     mix.version();
 }
+
+// Establecer la ruta pública por defecto para las fuentes de Font Awesome
+mix.webpackConfig({
+    output: {
+        publicPath: '/fonts/'
+    }
+});
